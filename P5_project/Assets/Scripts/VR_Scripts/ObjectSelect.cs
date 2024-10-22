ba9f4d2880ffd98b0c36d373672b9b1273fb8ee2
Simon <gulfurs@gmail.com>
2024-10-22 12:40:28 +0200

    more working as last push

---
+++ P5_project/Assets/Scripts/VR_Scripts/ObjectSelect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelect : MonoBehaviour
+{
+    public void OnObjectSelected()
+    {
+        Debug.Log("Object clicked");
+    }
+}
