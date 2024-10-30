56527ba635478357496edfb6826a6323ae9cee15
fijibottle <gulfurs@gmail.com>
2024-10-30 21:53:19 +0100

    added buttonclikc
    only in debug.log tho
    and only in testenv

--- P5_project/Assets/Scripts/VR_Scripts/ClickActions.cs
+++ P5_project/Assets/Scripts/VR_Scripts/ClickActions.cs
@@ -6,15 +6,21 @@ using UnityEngine.InputSystem;
 public class ClickActions : MonoBehaviour
 {
 
+    //The actionMap
     public InputActionAsset actionAsset;
 
+    //The different bindings
     private InputAction LeftGrip;
     private InputAction LeftTrigger;
     private InputAction RightGrip;
     private InputAction RightTrigger;
 
-    void OnEnable()
+    //ButtonOptinos
+    //public ButtonOption_A
+
+    void OnEnable() 
     {
+        //Connecting them to the code
         var actionMap = actionAsset.FindActionMap("XRActionMap"); 
         LeftGrip = actionMap.FindAction("XR_GripButton_Left");  
         LeftTrigger = actionMap.FindAction("XR_TriggerButton_Left");  
@@ -22,7 +28,8 @@ public class ClickActions : MonoBehaviour
         RightTrigger = actionMap.FindAction("XR_TriggerButton_Right");  
 
         // Subscribe to the actions
-        LeftGrip.performed += LeftGripClass;
+        LeftGrip.performed += OnTriggerPress;
+        //LeftGrip.performed += LeftGripClass;
         LeftTrigger.performed += LeftTriggerClass;
         RightGrip.performed += RightGripClass;
         RightTrigger.performed += GripTriggerClass;
@@ -34,9 +41,10 @@ public class ClickActions : MonoBehaviour
         RightTrigger.Enable();
     }
 
-    void OnDisable()
+    void OnDisable() //Oppersite of Enable
     {
-        LeftGrip.performed -= LeftGripClass;
+        LeftGrip.performed -= OnTriggerPress;
+        //LeftGrip.performed -= LeftGripClass;
         LeftTrigger.performed -= LeftTriggerClass;
         RightGrip.performed -= RightGripClass;
         RightTrigger.performed -= GripTriggerClass;
@@ -48,16 +56,26 @@ public class ClickActions : MonoBehaviour
         RightTrigger.Disable();
     }
 
+    //----The classes for each button - see if work. ----
     public void LeftGripClass(InputAction.CallbackContext context){
-        Debug.Log("test-gripLeft");
+       // Debug.Log("test-gripLeft");
     }
     public void LeftTriggerClass(InputAction.CallbackContext context){
-        Debug.Log("test_trigger_left");
+        //Debug.Log("test_trigger_left");
     }
     public void RightGripClass(InputAction.CallbackContext context){
-        Debug.Log("test_right_grip");
+        //Debug.Log("test_right_grip");
     }
     public void GripTriggerClass(InputAction.CallbackContext context){
-        Debug.Log("test_trigger_right");
+        //Debug.Log("test_trigger_right");
     }
+    //---END of testing----
+
+    //The triggerbutton trigger the button trigger :)
+    public void OnTriggerPress(InputAction.CallbackContext context)
+    {
+        Debug.Log("Button has been pressed!");
+        //
+    }
+
 }
