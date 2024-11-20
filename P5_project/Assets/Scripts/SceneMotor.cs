58c666d408a5f57b451ecf32e3f6e5e5186025da
Mufasakong <Halipha.haidary@gmail.com>
2024-11-20 13:48:10 +0100

    Experimenting with load stuff, before scenes
    Will hopefully remove lag! Prayge

--- P5_project/Assets/Scripts/SceneMotor.cs
+++ P5_project/Assets/Scripts/SceneMotor.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneMotor : MonoBehaviour
 {
+    private AsyncOperation preloadOperation;
+
     // Public method to load the next scene in the build index order
     public void NextScene()
     {
@@ -13,6 +16,34 @@ public class SceneMotor : MonoBehaviour
         int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
         
         // Load the next scene
-        SceneManager.LoadScene(nextSceneIndex);
+        //SceneManager.LoadScene(nextSceneIndex);
+
+        StartCoroutine(PreloadScene(nextSceneIndex));
+    }
+
+    private IEnumerator PreloadScene(int sceneIndex)
+    {
+        // PRELOAD SCENES ASYNC
+        preloadOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        preloadOperation.allowSceneActivation = false; 
+
+        // WAIT TILL SCENE IS LOADED
+        while (preloadOperation.progress < 0.9f)
+        {
+            // DISPLAY PROGRESS??
+            Debug.Log($"Loading progress: {preloadOperation.progress * 100}%");
+            yield return null;
+        }
+
+        Debug.Log("Scene preloaded but not activated.");
+    }
+
+    // ACTIVATE PRELOADED SCENE
+    public void ActivatePreloadedScene()
+    {
+        if (preloadOperation != null)
+        {
+            preloadOperation.allowSceneActivation = true;
+        }
     }
 }
